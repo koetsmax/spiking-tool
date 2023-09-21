afb5269bb5e0ae8f673fc9ef209d3156ff1a1c70
koetsmax <koetsmax@gmail.com>
2023-09-21 19:24:42 +0200

    Added missing regions

--- sot/Region.py
+++ sot/Region.py
@@ -12,13 +12,18 @@ def region_from_name(name):
 core_regions = {
     "US East - NY/NJ": Region("Tappahannock", "United States", "US-E"),
     "US Central - Chicago": Region("Chicago", "United States", "US-C"),
-    "US South - Dallas": Region("San Antonio", "United States", "US-S"),  # Dallas
+    "US South - Dallas": Region("San Antonio", "United States", "US-S"),
+    "US Midwest - Des Moines": Region("Des Moines", "United States", "US-MW"),
     "US West - LA/LV": Region("San Jose", "United States", "US-W"),
+    "Brazil": Region("Campinas", "Brazil", "BR"),
     "Europe - Amsterdam": Region("Amsterdam", "Netherlands", "EU-C"),
     "Europe - United Kingdom": Region("Dublin", "Ireland", "EU-W"),
+    "Dubai": Region("Dubai", "United Arab Emirates", "UAE"),
+    "South Africa": Region("Johannesburg", "South Africa", "ZA"),
     "Singapore": Region("Singapore", "Singapore", "SG"),
-    "Hong Kong": Region("Central", "Hong Kong", "CN"),
-    "Japan": Region("Tokyo", "Japan", "JP"),
-    "Sydney": Region("Sydney", "Australia", "OCE"),
-    "Brazil": Region("Campinas", "Brazil", "BR"),
+    "Hong Kong": Region("Hong Kong", "Hong Kong", "HK"),
+    "Japan - Tokyo": Region("Tokyo", "Japan", "JP1"),
+    "Japan - Osaka": Region("Osaka", "Japan", "JP2"),
+    "Australia - Sydney": Region("Sydney", "Australia", "OCE1"),
+    "Australia - Melbourne": Region("Melbourne", "Australia", "OCE2"),
 }
