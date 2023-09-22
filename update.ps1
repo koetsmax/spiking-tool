aed8a3bb1695e4d94e7816be3d164e9a7bc58dea
koetsmax <koetsmax@gmail.com>
2023-09-22 21:02:42 +0200

    Updated update script

--- update.ps1
+++ update.ps1
@@ -1,5 +1,7 @@
 # delete the file called Client.exe on the desktop and rename the file called TempClient.exe to Client.exe
 
 Start-Sleep -s 2
-Remove-Item -Path C:\Users\Public\Desktop\Client.exe
-Rename-Item -Path C:\Users\Public\Desktop\TempClient.exe -NewName Client.exe
+Remove-Item -Path C:\Users\max\Desktop\client.exe
+Rename-Item -Path C:\Users\max\Desktop\TempClient.exe -NewName client.exe
+# launch the file called Client.exe on the desktop
+Start-Process -FilePath C:\Users\max\Desktop\client.exe
